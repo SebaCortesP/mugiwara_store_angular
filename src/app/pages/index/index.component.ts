c25acc11be42d6d34a7b8b7fa0d877c768019c66
SebaCortesP <seba.cortesp@duocuc.cl>
2025-06-16 18:35:40 -0400

    fix redirect index

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [ HeroComponent, FooterComponent ],
+  imports: [ HeroComponent, FooterComponent, RouterModule ],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
